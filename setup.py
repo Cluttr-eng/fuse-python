3bec34fe4571d994013bd68407833447026ab79b
AdibFuse <adib@letsfuse.com>
2023-08-18 08:18:36 +0000

    Update fuse python

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "fuse-client"
-VERSION = "1.0.26"
+VERSION = "2.0.0"
 # To install the library, run the following
 #
 # python setup.py install
