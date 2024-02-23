9a383c5e8c75785a05896c38d1391a76ec8f8e48
AdibFuse <adib@letsfuse.com>
2024-02-23 12:04:02 +0000

    Update fuse python

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "fuse-client"
-VERSION = "2.0.2"
+VERSION = "2.0.3"
 # To install the library, run the following
 #
 # python setup.py install
@@ -25,7 +25,7 @@ REQUIRES = [
     "frozendict ~= 2.3.4",
     "python-dateutil ~= 2.8.0",
     "setuptools >= 21.0.0",
-    "typing_extensions ~= 4.3.0",
+    "typing_extensions >= 4.3.0, < 5.0.0",
     "urllib3 ~= 1.26.7",
 ]
 
